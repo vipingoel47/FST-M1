a74033ae02702ac69c6be4e2defcec2582c1d3c0
vipingoel47 <95232523+vipingoel47@users.noreply.github.com>
2021-11-29 11:31:52 -0500

    Add files via upload

---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,11 @@
+REM   Script: Activity5
+REM   Update table
+
+UPDATE salesman SET grade=200 where salesman_city='Rome';
+
+UPDATE salesman SET grade=300 where salesman_name='James Hoog';
+
+UPDATE salesman SET salesman_name='Pierre' where salesman_name='McLyon';
+
+SELECT * FROM salesman
+
