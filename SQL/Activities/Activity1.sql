50bc5d981042fd99285e0554e3e3f3dcc3086e9d
vipingoel47 <95232523+vipingoel47@users.noreply.github.com>
2021-11-29 11:15:55 -0500

    Add files via upload

---
+++ SQL/Activities/Activity1.sql
@@ -0,0 +1,10 @@
+REM   Script: Activity1
+REM   Create salesman table
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(20), 
+    salesman_city varchar2(20), 
+    commission int 
+);
+
