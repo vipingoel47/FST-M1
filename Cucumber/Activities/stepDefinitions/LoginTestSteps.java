b97f6f338a3e8c2ebbd9a7f15c3294a4bd08a287
vipingoel47 <95232523+vipingoel47@users.noreply.github.com>
2021-12-21 17:38:49 -0500

    Add files via upload

--- Cucumber/Activities/stepDefinitions/LoginTestSteps.java
+++ Cucumber/Activities/stepDefinitions/LoginTestSteps.java
@@ -18,7 +18,7 @@ public class LoginTestSteps {
     WebDriver driver;
     WebDriverWait wait;
     
-    @Given("^User is on the Login page$")
+    @Given("^User is on Login page$")
     public void loginPage() {
         //Setup instances
         driver = new FirefoxDriver();
@@ -28,7 +28,17 @@ public class LoginTestSteps {
         driver.get("https://www.training-support.net/selenium/login-form");
     }
     
-    @When("^User enters the \"(.*)\" and \"(.*)\"$")
+    @When("^User enters username and password$")
+    public void enterCredentials() {
+        //Enter username
+        driver.findElement(By.id("username")).sendKeys("admin");
+        //Enter password
+        driver.findElement(By.id("password")).sendKeys("password");
+        //Click Login
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    @When("^User enters \"(.*)\" and \"(.*)\"$")
     public void user_enters_and(String username, String password) throws Throwable {
         //Enter username from Feature file
         driver.findElement(By.id("username")).sendKeys(username);
@@ -38,7 +48,7 @@ public class LoginTestSteps {
         driver.findElement(By.xpath("//button[@type='submit']")).click();
     }
     
-    @Then("^Read page title and confirmation message$")
+    @Then("^Read the page title and confirmation message$")
     public void readTitleAndHeading() {
         wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("action-confirmation")));
         
@@ -49,13 +59,16 @@ public class LoginTestSteps {
         //Print the page title and heading
         System.out.println("Page title is: " + pageTitle);
         System.out.println("Login message is: " + confirmMessage);
-
-        //Assertion
-        Assert.assertEquals(confirmMessage, "Welcome Back, admin");
+        
+        if(confirmMessage.contains("admin")) {
+            Assert.assertEquals(confirmMessage, "Welcome Back, admin");
+        } else {
+            Assert.assertEquals(confirmMessage, "Invalid Credentials");
+        }
     }
     
-    @And("^Close the Browser4$")
-    public void closeBrowser4() {
+    @And("^Close the Browser$")
+    public void closeBrowser() {
         //Close browser
         driver.close();
     }
